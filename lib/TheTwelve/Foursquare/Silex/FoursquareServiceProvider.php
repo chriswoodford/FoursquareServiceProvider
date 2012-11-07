9c4623da75be62ba4b3099eb5a0d0bdf202492c5
Chris Woodford <chris.woodford@gmail.com>
2012-11-07 02:55:02 -0500

    basic service provider implementation

--- lib/TheTwelve/Foursquare/Silex/FoursquareServiceProvider.php
+++ lib/TheTwelve/Foursquare/Silex/FoursquareServiceProvider.php
@@ -1,1 +1,32 @@
 <?php
+
+namespace TheTwelve\Foursquare\Silex;
+
+use Silex;
+
+class FoursquareServiceProvider implements Silex\ServiceProviderInterface
+{
+
+    public function register(Application $app)
+    {
+
+        // TODO: instantiate http client
+
+        $app['foursquare'] = $app->share(function() use ($app) {
+
+            $client = $app['foursquare.client'];
+
+            $factory = new \TheTwelve\Foursquare\ApiGatewayFactory($client);
+            $factory->useVersion($app['foursquare.version']);
+            $factory->setEndpointUri($app['foursquare.endpoint']);
+
+            return $factory;
+
+        });
+
+    }
+
+    public function boot(Silex\Application $app)
+    {}
+
+}
